8caf756732cd7ce03f6653bcf44f6d6f2716f22d
HendrikHenselmann <hendrikh@freenet.de>
2022-07-30 11:58:01 +0200

    Set random seed in Main.

--- src/Main.c
+++ src/Main.c
@@ -1,3 +1,6 @@
+// Copyright [2022] <Nicola Distl, Hendrik Henselmann>
+
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +9,11 @@
 #include "../include/Mutation.h"
 
 int main() {
+    
+    // Set random seed. This should be done once for the the whole program!
+    // It is very important that this is the only place where srand is called!
+    srand(time(NULL));
+
     // Test Individuals
     Individual_t indi1 = calloc(NUM_ITEMS, sizeof(bool));
     Individual_t indi2 = malloc(NUM_ITEMS * sizeof(bool));
