5ee5a9aa61c0148c8959f97ac5ac75cab234dd67
HendrikHenselmann <hendrikh@freenet.de>
2022-07-31 03:08:51 +0200

    Removing theoretical runtime designation of elitism, because it could have the same runtime as sorting [O(n*log n)].

--- include/Elitism.h
+++ include/Elitism.h
@@ -7,7 +7,7 @@
 
 // Restructuring the population so that the first {numElitists} individuals of
 // the population are the individuals that will survive unmodified.
-// O(populationSize)
+// Restructuring the fitness score array accordingly.
 void applyElitism(Population_t population, size_t numElitists,
     FitnessScores_t fitnessScores);
 
