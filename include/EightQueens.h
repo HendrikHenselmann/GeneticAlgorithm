e45ce41d003f9f370804e3d2d77b3b7fb4ecd4e5
HendrikHenselmann <hendrikh@freenet.de>
2022-12-28 00:14:31 +0100

    Added the Eight Queens problem as environment. Found a few bugs and fixed some of them.

---
+++ include/EightQueens.h
@@ -0,0 +1,42 @@
+// Copyright [2022] <Nicola Distl, Hendrik Henselmann>
+
+#ifndef EIGHTQUEENS_H_
+#define EIGHTQUEENS_H_
+
+// ----------------------------------------------------------------------------
+// Imports
+// ----------------------------------------------------------------------------
+// Project headers
+#include "./DTypes.h"
+#include "./Environment.h"
+// ----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+// Functions
+// ----------------------------------------------------------------------------
+// Calculate the fitness function of an individual according to the problem
+float eightQueens_individualFitness (Individual_t individual);
+// Calculate the fitness function of the whole population according to the
+// problem. Writing the fitness into the fitnessScores struct.
+void eightQueens_populationFitness (Population_t population,
+    FitnessScores_t fitnessScores);
+// Display the problem
+void eightQueens_displayProblem(void);
+// Display an Individual interpreted according to the problem
+void eightQueens_displayIndividual (Individual_t individual);
+// Display the Population interpreted according to the problem
+void eightQueens_displayPopulation (Population_t population);
+// Calculate the optimal solution
+// Returning INT_MIN to indicate failure of malloc
+float eightQueens_calcOptimum (void);
+// ----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+// The exported environment
+// ----------------------------------------------------------------------------
+extern Environment_t eightQueensProblem;
+// ----------------------------------------------------------------------------
+
+#endif  // EIGHTQUEENS_H_
---
+++ src/Environments/EightQueens.c
@@ -0,0 +1,159 @@
+#include "../../include/EightQueens.h"
+
+#include <stdio.h>
+
+// Defining a seperation line
+static char separator[] = "=====================================================\
+=================";
+
+// Calculate the fitness function of an individual according to the problem
+// Counting the number of conflicting queens, the lesse the better
+float eightQueens_individualFitness (Individual_t individual) {
+
+    // Set num conflicts to the max number possible
+    unsigned numConflicts = 28; //4*7 = 28
+
+    // Initialize conflict counters for columns/rows/diagonals
+    unsigned xs[8] = {0};
+    unsigned ys[8] = {0};
+    unsigned z1s[15] = {0};
+    unsigned z2s[15] = {0};
+
+    // Convert the bit patterns to the (x, y) coordinate of the queens
+    // Then increment the number of conflicts per
+    // column/row/diagonal1/diagonal2
+    for (unsigned queenID = 0; queenID < 8; queenID++) {
+
+        // Extracting (x, y) coordinate of the current queen
+        unsigned xCoord = 0;
+        for (unsigned i = 0; i < 3; i++)
+            if (individual[6*queenID + i])
+                xCoord += 1 << i;
+
+        unsigned yCoord = 0;
+        for (unsigned i = 0; i < 3; i++)
+            if (individual[6*queenID + 3 + i])
+                yCoord += 1 << i;
+
+        // Calculate the group memberships of the current queen
+        xs[xCoord]++;
+        ys[yCoord]++;
+        z1s[xCoord+yCoord]++;
+        z2s[7-xCoord+yCoord]++;
+    }
+
+    // Subtract the number of conflicts
+    for (unsigned i = 0; i < 8; i++)
+        if (xs[i] > 0)
+            numConflicts -= xs[i] - 1;
+    for (unsigned i = 0; i < 8; i++)
+        if (ys[i] > 0)
+            numConflicts -= ys[i] - 1;
+    for (unsigned i = 0; i < 15; i++)
+        if (z1s[i] > 0)
+            numConflicts -= z1s[i] - 1;
+    for (unsigned i = 0; i < 15; i++)
+        if (z2s[i] > 0)
+            numConflicts -= z2s[i] - 1;
+
+    return (float) numConflicts;
+}
+
+// Calculate the fitness function of the whole population according to the
+// problem. Writing the fitness into the fitnessScores struct.
+void eightQueens_populationFitness (Population_t population,
+    FitnessScores_t fitnessScores) {
+    for (size_t indi = 0; indi < population->populationSize; indi++) {
+        fitnessScores->array[indi] =
+            eightQueens_individualFitness (population->array[indi]);
+    }
+}
+
+// Display the problem
+void eightQueens_displayProblem (void) {
+    // Print the heading and a short description
+    printf("\n\n%s", separator);
+    printf("\nThis is the 8 Queens problem");
+    printf("\n%s", separator);
+    printf("\nThe goal is to place 8 queens on an 8x8 tiled chess board.\n"
+    "There cannot be any conflicts emerging from the attack pattern\n"
+    "of classic chess."
+    );
+    printf("\n%s\n\n", separator);
+    return;
+}
+
+// Helper function for eightQueens_displayIndividual
+void printBoardSeperator (void) {
+    printf("\n\t");
+    for (int _ = 0; _ < 8; _++) printf("|---");
+    printf("|\n");
+}
+
+// Display an Individual interpreted according to the problem
+void eightQueens_displayIndividual (Individual_t individual) {
+
+    // Print the heading
+    printf("\n\n%s", separator);
+    printf("\nIndividual:");
+    printf("\n%s\n\n", separator);
+
+    // Extract board out of individuals representation
+    bool board[64] = {false};
+
+    // Insert every queen into board
+    for (unsigned queenID = 0; queenID < 8; queenID++) {
+
+        // Extracting (x, y) coordinate of the current queen
+        unsigned xCoord = 0;
+        for (unsigned i = 0; i < 3; i++) if (individual[6*queenID + i])
+            xCoord += 1 << i;
+
+        unsigned yCoord = 0;
+        for (unsigned i = 0; i < 3; i++) if (individual[6*queenID + 3 + i])
+            yCoord += 1 << i;
+
+        // Insert into board
+        board[xCoord + 8*yCoord] = true;
+    }
+
+    // Print the chess board that is represented by the genes
+    printBoardSeperator ();
+    for (unsigned y = 0; y < 8; y++) {
+        printf("\t");
+        for (unsigned x = 0; x < 8; x++) {
+            printf("| ");
+            if (board[x+8*y]) printf("x"); else printf(" ");
+            printf(" ");
+        }
+        printf("|");
+        printBoardSeperator ();
+    }
+
+    // Print the number of conflicts (fitness value)
+    printf("\n%s", separator);
+    float indiFitness = eightQueens_individualFitness(individual);
+    printf("\nconflicts: %02.f of 27, fitness: %02.f",
+        28 - indiFitness,
+        indiFitness
+        );
+    printf("\n%s\n\n", separator);
+
+    return;
+}
+
+// Display the Population interpreted according to the problem
+void eightQueens_displayPopulation (Population_t population) {}
+
+// Calculate the optimal solution
+// Returning INT_MIN to indicate failure of malloc
+float eightQueens_calcOptimum (void) {}
+
+// Assembling the environment
+Environment_t eightQueensProblem = (Environment_t) {
+    .individualSize = 48,  // (x, y) coordinates of 8 Queens, x and y in [0, 7] encoded by 3 bit => 8 * 2 * 3 = 48
+    .displayProblem = eightQueens_displayProblem,
+    .displayIndividual = eightQueens_displayIndividual,
+    .displayPopulation = eightQueens_displayPopulation,
+    .calcIndividualFitness = eightQueens_individualFitness,
+    .calcPopulationFitness = eightQueens_populationFitness};
